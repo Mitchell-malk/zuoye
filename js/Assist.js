b504691c97e33eac1ae45aad0a3b2cddb50662c7
Mitchell-malk <a2528242133@163.com>
2021-11-15 17:54:05 +0800

    修改Assist.js

--- js/Assist.js
+++ js/Assist.js
@@ -1,30 +1,19 @@
 /**
  * 第一题
  * */
-
-$(function (){
-
-    $(".Add").on('click',function () {
-        let a = $('[type=text]').val();
-        //没填写内容
-        if (a.length === 0){
-            alert("还没填写内容！！！");
-        }else {
-            //删除value
-            $('.input').val(function () {
-                return this.value = '';
-            });
-            //在输入框下面添加内容
-            // let x=$('<li>');
-            $('ol').prepend(`<li><span>x</span>  ${a}</li>`);
-            $('span').click(function(){
-                $(this).parent().remove();
-            }) ;
-        }
-        // 清空列表
-        $('.Empty').click(function () {
-            $('ol').remove();
+const input = $('input').eq(0);
+const addBtn = $('.Add');
+const list = $('ol').eq(0);
+const cleanBtn = $('.Empty');
+addBtn.click(function () {
+    let val = input.val();
+    if (val === ''){
+        alert('还没填写内容');
+    }else {
+        input.val('');
+        list.prepend(`<li><span>&times;</span> ${val}</li>`);
+        list.find('ol span').click(function () {
+           $(this).parent().remove();
         });
-    });
-    $('p').prepend(`1C, 2B, 3C, 4D, 5D, 6B, 7C, 8D, 9A, 10A, 11C, 12A, 13B, 14D, 15B, 16C, 17A, 18B, 19A, 20D `);
-});
+    }
+})
